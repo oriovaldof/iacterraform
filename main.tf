8124daa496ced51f050b76a8e670539a09ecad1e
Oriovaldo Fialho <oriovaldof@gmail.com>
2022-08-07 16:35:14 -0400

    resoure e file

--- main.tf
+++ main.tf
@@ -23,3 +23,24 @@ resource "digitalocean_kubernetes_cluster" "k8s_iniciativa" {
     node_count = 3
   }
 }
+
+resource "digitalocean_kubernetes_node_pool" "node_premium" {
+  cluster_id = digitalocean_kubernetes_cluster.k8s_iniciativa.id
+
+  name       = "premium"
+  size       = "s-4vcpu-8gb"
+  node_count = 2
+}
+
+variable "do_token" {}
+variable "k8s_name" {}
+variable "region" {}
+
+output "kube_endpoint" {
+  value = digitalocean_kubernetes_cluster.k8s_iniciativa.endpoint
+}
+
+resource "local_file" "kube_config" {
+  content = digitalocean_kubernetes_cluster.k8s_iniciativa.kube_config.0.raw_config
+  filename = "kube_config.yaml"
+}
